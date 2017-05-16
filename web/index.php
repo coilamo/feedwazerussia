e9d906a40396b513a6eebf7077563099df02f8db
Vasilii <alexletov@users.noreply.github.com>
2017-05-16 19:05:41 +0300

    Update index.php
    Fix YII_ENV

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', false);
-defined('YII_ENV') or define('YII_ENV', 'production');
+defined('YII_ENV') or define('YII_ENV', 'prod');
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
