973c68d9fd64456fb3ef2948a94100ead594fa8b
Vasilii <alexletov@users.noreply.github.com>
2017-05-17 15:53:43 +0300

    Update index.php
    One more wording qfix

--- views/feed/index.php
+++ views/feed/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a(Yii::t('app', 'Create Feed'), ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a(Yii::t('app', 'Create Report'), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 <?php Pjax::begin(); ?>    <?= GridView::widget([
         'dataProvider' => $dataProvider,
